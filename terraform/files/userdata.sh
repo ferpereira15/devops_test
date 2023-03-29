da69e142fc9afa68a1b8f1cbb3f11ad8d3640463
Fernanda <fernanda_pereira@llive.com>
2023-03-29 00:09:36 -0300

    fix userdata file

--- terraform/files/userdata.sh
+++ terraform/files/userdata.sh
@@ -8,7 +8,7 @@ systemctl start docker
 systemctl enable docker
 mkdir /tmp/webservice-devopsteste
 cd /tmp/webservice-devopsteste
-git clone https://github.com/ferpereira15/devops_test.git
+git clone https://github.com/ferpereira15/devops_test.git;
 cd devops-test
 docker build . -t web_app
 docker run -p 8080:8080 web_app
