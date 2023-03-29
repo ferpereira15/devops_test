859367a78dd8a7147f710bde3c68007e07260a39
Fernanda <fernanda_pereira@llive.com>
2023-03-28 22:40:32 -0300

    adding terraform files

---
+++ terraform/securitygroup.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "main_security_public" {
+  name        = "public"
+  description = "Allow HTTP public access"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group" "main_security_remote" {
+  name        = "remote"
+  description = "Allow remote SSH access"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
